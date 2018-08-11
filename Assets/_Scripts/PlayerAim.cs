c5e87b5ec7efee01d8bdbdc5348ad4166655af13
MasterKrepta <brianj.pajak@gmail.com>
2018-08-11 00:06:21 -0400

    Cooldown added

--- Assets/_Scripts/PlayerAim.cs
+++ Assets/_Scripts/PlayerAim.cs
@@ -6,11 +6,16 @@ public class PlayerAim : MonoBehaviour {
 
     [SerializeField] Transform gun;
     [SerializeField] Transform bullet;
+    [SerializeField] float fireRate;
+    float cooldown;
+    bool canFire = true;
     public float visualOffset;
     Animator anim;
+
 	// Use this for initialization
 	void Start () {
         anim = Camera.main.GetComponent<Animator>();
+        cooldown = fireRate;
 	}
 	
 	// Update is called once per frame
@@ -19,9 +24,18 @@ public class PlayerAim : MonoBehaviour {
         float rotation = Mathf.Atan2(dif.y, dif.x) * Mathf.Rad2Deg;
         gun.transform.rotation = Quaternion.Euler(0, 0, rotation + visualOffset);
 
-        if (Input.GetMouseButton(0)) {
+        if (Input.GetMouseButton(0) && canFire) {
+            canFire = false;
             anim.SetTrigger("Fired");
             Instantiate(bullet, gun.position, gun.rotation);
+        }
+
+        if (!canFire && cooldown > 0) {
+            cooldown -= Time.deltaTime;
+        }
+        else {
+            canFire = true;
+            cooldown = fireRate;
         }
 	}
 }
